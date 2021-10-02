a1ff8f22ce2435e01420382cac9eb9243c531ca9
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-10-02 12:08:41 -0300

    feat: basic cases of assertion
    Semantic assertion basic cases covered

--- test/test-spect-expect.js
+++ test/test-spect-expect.js
@@ -5,6 +5,14 @@ test('Expect should have a facade for execution', () => {
   assert.ok(expect('foo') instanceof Expect);
 });
 
-test('Expect should have semantic equal comparison', () => {
+test('Expect should have semantic strict equal comparison', () => {
   expect('foo').toStrictEqual('foo');
+});
+
+test('Expect should have a to be truthy', () => {
+  expect(true).toBeTruthy();
+});
+
+test('Expect should have a deep equal', () => {
+  expect({ a: 1, b: 2, c: 3 }).toStrictEqual({ a: 1, b: 2, c: 3});
 });
